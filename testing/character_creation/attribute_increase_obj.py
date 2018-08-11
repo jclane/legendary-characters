34eccf2a4ad2d54b65f48bbb0ff0471dccdaf483
Justin <j.lane@twc.com>
2018-08-11 11:23:47 -0400

    Started skeleton for character as object

--- testing/character_creation/attribute_increase_obj.py
+++ testing/character_creation/attribute_increase_obj.py
@@ -1,4 +1,16 @@
-class character:
-  def __init__(self, name, stats):
+class CharacterSheet:
+  def __init__(self, name, description, stats, banes, boons, feats):
     self.name = name
+    self.description = description
     self.stats = stats
+    self.banes = banes
+    self.boons = boons
+    self.feats = feats
+
+cindy = CharacterSheet(name='cindy', description='pretty lady', 
+stats={'physical':{'might':0, 'agility':0, 'fortitude':0}, \
+      'mental':{'learning':0}}, banes='potatoes', boons='unicorn 
+mastery', 
+feats='unicorn potato attack')
+
+print(cindy)
