63a29aea9d67309ab8436cf08dea9e3402333d9d
jclane <justin@justinlane.me>
2018-08-13 21:19:19 -0400

    Renamed some functions according to PEP

--- GUI/main.py
+++ GUI/main.py
@@ -5,25 +5,21 @@ from tkinter import ttk
 class tkWindow:
 
     def __init__(self, root):
-        
-        def doNew():
+        def do_new():
             pass
-        
-        def doSave():
+
+        def do_save():
             pass
-        
-        def doPrint():
+
+        def do_print():
             pass
-        
-        def doPrint():
-            pass
-        
+
         menubar = Menu(root)
         menu_file = Menu(menubar, tearoff=0)
         menubar.add_cascade(menu=menu_file, label='File')
-        menu_file.add_command(label='New', command=doNew)
-        menu_file.add_command(label='Save', command=doSave)
-        menu_file.add_command(label='Print', command=doPrint)
+        menu_file.add_command(label='New', command=do_new)
+        menu_file.add_command(label='Save', command=do_save)
+        menu_file.add_command(label='Print', command=do_print)
         menu_file.add_separator()
         menu_file.add_command(label='Quit', command=root.destroy)
         
@@ -56,7 +52,8 @@ class tkWindow:
         ttk.Entry(fluffframe, textvariable=race).grid(row=1, column=7, columnspan=3, sticky=W + E)
 
         Label(fluffframe, text="Size").grid(row=1, column=10, sticky=W)
-        ttk.Combobox(fluffframe, textvariable=size, values=("Small", "Medium", "Large")).grid(row=1, column=11, columnspan=3, sticky=W + E)
+        ttk.Combobox(fluffframe, textvariable=size, values=("Small", "Medium", "Large")) \
+            .grid(row=1, column=11, columnspan=3, sticky=W + E)
         
         Label(fluffframe, text="Physical Trait 1").grid(row=2, column=1, sticky=W)
         ttk.Entry(fluffframe, textvariable=physical_trait1).grid(row=2, column=2, columnspan=13, sticky=W + E)
